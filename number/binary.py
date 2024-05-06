3156c167a4d600470c0f36af97181143d86e3f88
arief kurniawan <arief.siklus@gmail.com>
2024-05-06 16:54:10 +0700

    binary

---
+++ number/binary.py
@@ -0,0 +1,5 @@
+angka = 140
+angka_biner = 0b10001100
+
+if angka == angka_biner:
+    print(f"angka {angka} sama dengan biner {angka_biner:b}")
---
+++ number/integer.py
@@ -0,0 +1,4 @@
+angka1 = 24
+angka2 = 13
+total = angka1 + angka2
+print(f"angka: {total}")
